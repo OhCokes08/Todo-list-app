c96dc2a7f158a01256690d1c03f706dc43655b0a
obafemicoker <femiecoker@gmail.com>
2024-01-16 18:07:36 +0000

    Desktop GUI, web file and function files for the todo list

---
+++ functions.py
@@ -0,0 +1,18 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """"
+    Reads the todo's from a text file.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """"
+    Writes a todo item or list in a text file.
+    """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
